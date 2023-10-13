6210197104f6ec350bb39ea54e28aa1372d5439e
adrienfomat <azanguefomatgide@gmail.com>
2023-10-14 00:05:18 +0200

    Update CalculE.c

--- CalculE.c
+++ CalculE.c
@@ -1,63 +1,53 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-/*************************************************/
-/*                                               */
-/*                sucre syntaxique               */
-/*                                               */
-/*************************************************/
-
-#define AND &&
-#define OR ||
-#define ISNOT !=
-#define NOT !
-#define then 
-
-typedef enum { false, true} bool ;
-
-
-
-
-/*************************************************/
-/*                                               */
-/*            Calcul de e                        */
-/*                                               */
-/*************************************************/
-
-float fact = 1;
-int k = 20;
-const long double E = 2.719288;
-
 
 float Efloat () 
 { 
-    
-    float e = 1.0f; 
+    double e =  1 ; double  n = 1 ; double  temp = 1 ; double  val = 2.7182818284 ;
 
-    
-        for(int i=1; i<=k; i++)
-        {
-            fact = fact*i;
-            e += 1.0/fact;
-        }
+    do 
+    {
+        temp = temp / n ;
+        e += temp ;   
+        n = n + 1 ;
+    } while( val - e > 0) ;
     
     return e ; 
 }
 
-/*************************************************/
+double Edouble () 
+{ 
+    double e =  1 ; double  n = 1 ; double  temp = 1 ; double  val = 2.7182818284 ;
 
-//double Edouble () {retour 0 ; }
-  
-/*************************************************/
+    do 
+    {
+        temp = temp / n ;
+        e += temp ;   
+        n = n + 1 ;
+    } while( val - e > 0) ;
+    
+    return e ; 
+}
 
-//long double Elongdouble () { return 0 ; }
 
+long double Elongdouble () 
+{ 
+    long double e =  1 ; long double  n = 1 ; long double  temp = 1 ; long double  val = 2.7182818284 ;
 
+    do 
+    {
+        temp = temp / n ;
+        e += temp ;   
+        n = n + 1 ;
+    } while( val - e > 0) ;
+    
+    return e ; 
+}
 
-int main(int argc, char** argv)
-{
-    printf(" -> %f",Efloat());
-    // printf("go");
-    return 0 ;
+int main() {
+   
+    printf("la somme est  %.15f \n",Efloat());
+    printf("la somme est  %.15f \n",Edouble());
+    printf("la somme est  %.15Lf \n",Elongdouble());
+
+    return 0;
 }
